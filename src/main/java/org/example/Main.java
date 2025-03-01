1758fa2f08bc90c7d5f3af03db660bf4b26d7108
BaoCoder288203 <baon6777@gmail.com>
2025-03-01 08:37:08 +0700

    Commit DP StaticBlock

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
 import org.example.Singleton.Eager;
+import org.example.Singleton.StaticBlock;
 
 public class Main {
     public static void main(String[] args) {
-        Eager eager = Eager.getInstance();
-        System.out.println(eager);
+//        Eager eager = Eager.getInstance();
+//        System.out.println(eager);
+
+        StaticBlock staticBlock = StaticBlock.getInstance();
+        System.out.println(staticBlock.hashCode());
     }
 }
--- src/main/java/org/example/Singleton/StaticBlock.java
+++ src/main/java/org/example/Singleton/StaticBlock.java
@@ -1,2 +1,20 @@
-package org.example.Singleton;public class StaticBlock {
+package org.example.Singleton;
+
+public class StaticBlock {
+    private static StaticBlock instance;
+
+    private StaticBlock(){}
+
+    //static block initialization for exception handling
+    static{
+        try{
+            instance = new StaticBlock();
+        }catch(Exception e){
+            throw new RuntimeException("Exception occured in creating singleton instance");
+        }
+    }
+
+    public static StaticBlock getInstance(){
+        return instance;
+    }
 }
